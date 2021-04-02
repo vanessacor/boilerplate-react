21d8b997ef00ed11b1a5024e8fa27f24b8a66bad
vanessa <vanessapcbarros@gmail.com>
2021-04-02 11:48:04 +0200

    Feat: install and config eslint and prettier with airbnb style guide

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function App() {
-   return (
-     <div className="App">
-        <h1>Hello World</h1>
-     </div>
-   );
- }
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
 export default App;
